6556064030066e4e69daec0eb0b9c6a4a85425b5
Dan Engel <dengel29@gmail.com>
2018-01-23 18:38:23 +0800

    api connects, index works

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -1,5 +1,19 @@
 class Api::V1::CampaignsController < Api::V1::BaseController
+  before_action :set_restaurant, only: [ :show ]
+
+  def show
+  end
+
   def index
     @campaigns = policy_scope(Campaign)
+    @campaigns = Campaign.all
   end
+
+  private
+
+  def set_restaurant
+    @Campaign = Campaign.find(params[:id])
+    authorize @Campaign  # For Pundit
+  end
+
 end
--- app/views/api/v1/campaigns/index.json.jbuilder
+++ app/views/api/v1/campaigns/index.json.jbuilder
@@ -1,8 +1,3 @@
 json.array! @campaigns do |campaign|
   json.extract! campaign, :id, :title, :content, :user_id, :status
-
-  t.string :title
-      t.text :content
-      t.references :user, foreign_key: true
-      t.string :status
 end
